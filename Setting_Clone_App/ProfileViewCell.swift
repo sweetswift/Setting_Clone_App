085af3ae2fb4410cf1dc8dc98bedbc1ed73eb0e4
LeeChungWoo <ios.lllccww@gmail.com>
2022-12-26 23:13:23 +0900

    [ADD]  설정화면 상단 프로필cell 추가

---
+++ Setting_Clone_App/ProfileViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewCell.swift
+//  Setting_Clone_App
+//
+//  Created by chungwoolee on 2022/12/26.
+//
+
+import UIKit
+
+class ProfileViewCell: UITableViewCell {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
+
