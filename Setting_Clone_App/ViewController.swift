fd6fc87168dd497b61d6ef48a9cdfb76b53c778d
LeeChungWoo <ios.lllccww@gmail.com>
2022-12-27 22:49:05 +0900

    [ADD] SettingModel 추가

--- Setting_Clone_App/ViewController.swift
+++ Setting_Clone_App/ViewController.swift
@@ -9,7 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var settingModel = [[SettingModel]]()
+    
     @IBOutlet weak var settingTableView: UITableView!
+    
+    func makeData() {
+        settingModel.append([SettingModel(leftImageName: "person.circle", menuTitle: "Sign in to your iPhone", subTitle: "Set up iCloud, the App Store, and more.", rightImageName: nil)]
+        )
+        settingModel.append([SettingModel(leftImageName: "gear", menuTitle: "일반", subTitle: nil, rightImageName: "chevron.right"),
+                             SettingModel(leftImageName: "person.fill", menuTitle: "손쉬운 사용", subTitle: nil, rightImageName: "chevron.right"),
+                             SettingModel(leftImageName: "hand.raised.fill", menuTitle: "개인정보 보호 및 보안", subTitle: nil, rightImageName: "chevron.right")])
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,8 +39,12 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 5
+        return settingModel[section].count
         
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return settingModel.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -44,7 +59,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         
         return cell
         
-      
+        
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
