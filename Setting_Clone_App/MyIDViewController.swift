fa8d1556b0e00970a0b88409b435ed0ec0730d5e
LeeChungWoo <ios.lllccww@gmail.com>
2022-12-28 23:57:50 +0900

    [UPDATE]  "iPhone에 로그인하기"  버튼액션 및 UI 완성

--- Setting_Clone_App/MyIDViewController.swift
+++ Setting_Clone_App/MyIDViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 class MyIDViewController: UIViewController {
-
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton! {
+        didSet {
+            nextButton.isEnabled = false
+        }
+    }
+    @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        emailTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+        
     }
-
+    
+    @objc func textFieldDidChange(sender: UITextField) {
+    
+        
+        if sender.text?.isEmpty == true {
+            nextButton.isEnabled = false
+        } else {
+            nextButton.isEnabled = true
+        }
+    }
+    @IBAction func doCancel(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
 }
